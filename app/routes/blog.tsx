ca9181ca595902ec6aa6942c2b0e93d40c575c75
Nafas Ebrahimi <ebranafas@gmail.com>
2025-01-02 21:43:38 +0330

    feat: use loader to handle rerender data

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const blogPosts = [
-  { id: 1, title: 'First Blog Post' },
-  { id: 2, title: 'Second Blog Post' },
-  { id: 3, title: 'Third Blog Post' },
-];
+export async function loader() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const blogPosts = await response.json();
+  return {
+    data: blogPosts,
+  };
+}
 
-const Blog: React.FC = () => {
+export default function Blog ({ loaderData }: { loaderData: {data: any[]} })  {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Blog</h1>
@@ -18,7 +20,7 @@ const Blog: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          {blogPosts.map((post) => (
+          {loaderData.data.map((post) => (
             <tr key={post.id}>
               <td className="py-2 px-4 border-b">{post.id}</td>
               <td className="py-2 px-4 border-b">{post.title}</td>
@@ -29,5 +31,3 @@ const Blog: React.FC = () => {
     </div>
   );
 };
-
-export default Blog;
